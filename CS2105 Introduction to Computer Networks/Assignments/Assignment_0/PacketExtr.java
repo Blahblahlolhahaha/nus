8894081ca48eeda51907191f8acb60623b24d432
Jess <pikachuuu1436@gmail.com>
2025-01-23 09:26:19 +0800

    fixed some bugs

--- CS2105 Introduction to Computer Networks/Assignments/Assignment_0/PacketExtr.java
+++ CS2105 Introduction to Computer Networks/Assignments/Assignment_0/PacketExtr.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 class PacketExtr{
     public static void main(String[] args){
-       byte[] data = new byte[1024];
+       byte[] data = new byte[20];
        int read;
        try{
            while((read = System.in.read(data)) != -1){
@@ -10,18 +10,23 @@ class PacketExtr{
                }
                int packetSize = 0;
                int sizeData = 0; 
-               for(int i = 6;i < 1024; i++){
+               for(int i = 6;i < 20; i++){
                   if(data[i] == 'B'){
-                     sizeData = 1024 - i - 1;
+                     sizeData = 20 - i - 1;
                      for(int x = 0;i - x > 6 ;x++){
                          packetSize += (data[i - 1 - x]-'0') * Math.pow(10,x);
                      }
-                     System.out.write(data,i + 1, 1024 - i - 1);
+                     if(packetSize < 20){
+                        System.out.write(data,i + 1,packetSize); 
+                     }
+                     else{
+                        System.out.write(data,i + 1, 20 - i - 1);
+                     }
                      System.out.flush();
                      break;
                   }
                }
-               if(packetSize < 1024){
+               if(packetSize < 20){
                    continue;
                }
                byte[] rest = new byte[packetSize - sizeData];
