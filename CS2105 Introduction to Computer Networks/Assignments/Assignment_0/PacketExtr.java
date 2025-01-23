17cf4dae4e7b1775da72964370f786b3174d7c24
Jess <pikachuuu1436@gmail.com>
2025-01-23 22:15:52 +0800

    fixed even more bugs

--- CS2105 Introduction to Computer Networks/Assignments/Assignment_0/PacketExtr.java
+++ CS2105 Introduction to Computer Networks/Assignments/Assignment_0/PacketExtr.java
@@ -1,38 +1,25 @@
 import java.io.IOException;
 class PacketExtr{
     public static void main(String[] args){
-       byte[] data = new byte[20];
        int read;
+       String sizeStr = "";
        try{
-           while((read = System.in.read(data)) != -1){
-               if(data[0] != 'S'){
-                   break;
-               }
+           while((read = System.in.read()) != -1){
                int packetSize = 0;
-               int sizeData = 0; 
-               for(int i = 6;i < 20; i++){
-                  if(data[i] == 'B'){
-                     sizeData = 20 - i - 1;
-                     for(int x = 0;i - x > 6 ;x++){
-                         packetSize += (data[i - 1 - x]-'0') * Math.pow(10,x);
-                     }
-                     if(packetSize < 20){
-                        System.out.write(data,i + 1,packetSize); 
-                     }
-                     else{
-                        System.out.write(data,i + 1, 20 - i - 1);
-                     }
-                     System.out.flush();
-                     break;
+               if(read == 'B'){
+                  char number[] = sizeStr.split(": ")[1].toCharArray();
+                  for(int i = number.length - 1;i >= 0; i--){
+                      packetSize += (number[i] - '0') * Math.pow(10,(number.length - 1) - i);
                   }
+                  byte[] rest = new byte[packetSize];
+                  System.in.read(rest);
+                  sizeStr = ""; 
+                  System.out.write(rest);
+                  System.out.flush();  
                }
-               if(packetSize < 20){
-                   continue;
-               }
-               byte[] rest = new byte[packetSize - sizeData];
-               System.in.read(rest); 
-               System.out.write(rest);
-               System.out.flush();     
+              else{
+                 sizeStr += (char)read;
+              } 
            }
        }catch(IOException e){
            System.out.println("Smth went wrong");
