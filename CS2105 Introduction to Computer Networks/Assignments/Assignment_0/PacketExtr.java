b98b1caae62fb0c6ccff303c392ed4cc6d668af2
Jess <pikachuuu1436@gmail.com>
2025-01-23 09:15:52 +0800

    done with assignment 0

--- CS2105 Introduction to Computer Networks/Assignments/Assignment_0/PacketExtr.java
+++ CS2105 Introduction to Computer Networks/Assignments/Assignment_0/PacketExtr.java
@@ -1,25 +1,36 @@
-import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 class PacketExtr{
     public static void main(String[] args){
-       ByteArrayOutputStream baos = new ByteArrayOutputStream();
        byte[] data = new byte[1024];
        int read;
-       while((read = System.in.read(data)) != -1){
-           int packetSize = 0;
-           int sizeData = 0; 
-           for(int i = 6;i < 1024; i++){
-              if(data[i] == 'B'){
-                 sizeData = i;
-                 for(int x = 0;i - x > 6 ;x++){
-                     packetSize += data[i - 1 - x] * Math.pow(10,x);
-                 }
-                 System.out.write(data,i+1, 1024 - i);
-                 System.out.flush();
-              }
+       try{
+           while((read = System.in.read(data)) != -1){
+               if(data[0] != 'S'){
+                   break;
+               }
+               int packetSize = 0;
+               int sizeData = 0; 
+               for(int i = 6;i < 1024; i++){
+                  if(data[i] == 'B'){
+                     sizeData = 1024 - i - 1;
+                     for(int x = 0;i - x > 6 ;x++){
+                         packetSize += (data[i - 1 - x]-'0') * Math.pow(10,x);
+                     }
+                     System.out.write(data,i + 1, 1024 - i - 1);
+                     System.out.flush();
+                     break;
+                  }
+               }
+               if(packetSize < 1024){
+                   continue;
+               }
+               byte[] rest = new byte[packetSize - sizeData];
+               System.in.read(rest); 
+               System.out.write(rest);
+               System.out.flush();     
            }
-           byte[] rest = new byte[packetSize - sizeData];
-           System.in.read(rest); 
-           
+       }catch(IOException e){
+           System.out.println("Smth went wrong");
        }
     }
 
