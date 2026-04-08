f6d328708c5a4e79a2b0bfe7b86063a6643b31e4
pikapika <pikachuuu1436@gmail.com>
2026-04-08 18:29:21 +0800

    more model

---
+++ CS2109_Introduction_to_AI_and_Machine_Learning/capstone-project/split.py
@@ -0,0 +1,36 @@
+import os
+import shutil
+import random
+
+src = "data/new_training"
+train_dst = "data/train"
+val_dst = "data/val"
+test_dst = "data/test"
+
+random.seed(42)
+
+for class_name in os.listdir(src):
+    class_path = os.path.join(src, class_name)
+    if not os.path.isdir(class_path):
+        continue
+
+    images = os.listdir(class_path)
+    random.shuffle(images)
+
+    n = len(images)
+    n_val = int(n * 0.1)
+    n_test = int(n * 0.1)
+
+    val_images = images[:n_val]
+    test_images = images[n_val:n_val + n_test]
+    train_images = images[n_val + n_test:]
+
+    for dst, split in [(train_dst, train_images), (val_dst, val_images), (test_dst, test_images)]:
+        os.makedirs(os.path.join(dst, class_name), exist_ok=True)
+        for img in split:
+            shutil.copy(
+                os.path.join(class_path, img),
+                os.path.join(dst, class_name, img)
+            )
+
+print("Done splitting!")
